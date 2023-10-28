a6c41605025e5a0b45d073b22a3022ff011d1cec
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-10-28 11:09:06 -0700

    Update CourseDetails.jsx

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const CourseDetails = ({ courseList }) => {
     
     const { courseId } = useParams();
+    const navigate = useNavigate();
     const course = courseList.filter(course => course.id == courseId)[0]
    
+
+    const handleEnroll = () => {
+      alert(`Congrats! You are enrolled in ${course.name}!`);
+      navigate("/courses");
+    }
+
+
     return (
     <div>
         <h3>Course Details</h3>
@@ -13,6 +21,9 @@ const CourseDetails = ({ courseList }) => {
         <h4>Course ID: {course.id}</h4>
         <h4>Course Name: {course.name}</h4>
         <h4>Course Description: {course.description}</h4>
+    
+        <button onClick={handleEnroll}>Enroll</button>
+    
     </div>
   )
 }
